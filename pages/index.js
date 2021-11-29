b47521e0563d5642c67652ef1d7176a4beca4f83
Michael Su <michaelsu.jobs@gmail.com>
2021-11-28 19:07:09 -0800

    Setup Layout and implement Chakra UI

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import { Box, Container, Heading } from '@chakra-ui/react';
+
 const Page = () => {
   return (
-    <div>
-      <h1>SHEEEEEEEEEEEEEEESH</h1>
-      Testing to see if site is deployed on Netlify
-    </div>
+    <Container>
+      <Box align="center" bg="lightgrey" borderRadius="lg" mb={6} p={3}>
+        Hello! My name is Michael, nice to meet you! 
+      </Box>
+
+      <Box display='flex'>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant='page-title'>
+            Michael Su
+          </Heading>
+          <p>(Developer / Photographer)</p>
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
